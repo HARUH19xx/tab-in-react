4eb7d8a966c04fa5e18f317493ebc985250b5852
H.Hokari <hookhull@gmail.com>
2022-01-19 01:55:44 +0900

    add iteration

--- src/components/Tab1.jsx
+++ src/components/Tab1.jsx
@@ -1,9 +1,20 @@
 import React from "react";
 
 const Tab1 = () => {
+    const pronouns = [
+        'you', 'I', 'he', 'she', 'they'
+    ]
+    const body = () => {
+        for (let index = 0; index <= pronouns.length; index++) {
+            return (
+                <div key={index}>{pronouns}</div>
+            )
+        }
+    }
     return (
         <div>
             <p>Hello!</p>
+            {body()}
         </div>
     )
 }
--- src/components/Tab2.jsx
+++ src/components/Tab2.jsx
@@ -1,9 +1,19 @@
 import React from "react";
 
 const Tab2 = () => {
+    const pronoms = [
+        'je', 'tu', 'il', 'elle', 'nous', 'vous', 'ils', 'elle'
+    ]
+    const corps = () => {
+        for (let index = 0; index <= pronoms.length; index++)
+        return (
+            <div key={index}>{pronoms}</div>
+        )
+    }
     return (
         <div>
             <p>Bonjour!</p>
+            {corps()}
         </div>
     )
 }
